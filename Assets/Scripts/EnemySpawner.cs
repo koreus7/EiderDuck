f256e7e335a379d8dcf1c91d20e0e8f7f23251c3
Leo Mahon <lm1249@york.ac.uk>
2015-12-03 18:33:51 +0000

    EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 
+	public float spawnPeriod = 0.2f;
+
+	private float _spawnPeriodCounter = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +14,23 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (_spawnPeriodCounter < spawnPeriod) 
+		{
+			_spawnPeriodCounter += Time.deltaTime;
+		} 
+		else 
+		{
+			_spawnPeriodCounter = 0;
+			Spawn();
+		}
+
 	
 	}
 
 	void Spawn ()
 	{
-	//	GameObject instance = Instantiate<GameObject>(Resources.Load("Enemy"));
-
+		GameObject instance = (GameObject)Instantiate(Resources.Load("Enemy"));
+		instance.transform.position = transform.position;
 	}
 }
