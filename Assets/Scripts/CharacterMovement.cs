a3e3c533f5a879df5ca9cd0bb97b36e354224761
Leo Mahon <lm1249@york.ac.uk>
2015-12-04 18:33:13 +0000

    Swim and Walk Speeds
    Press P to switch permanently to swim mode.
    
    Also updated mlnodevelop settings for out code style guidelines.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,25 +5,53 @@ public class CharacterMovement : MonoBehaviour {
 
 	Rigidbody2D _rigidBody;
 
+	public Rigidbody2D walkingRigidBody;
+	public Rigidbody2D swimmingRigidBody;
+
+	public float normalDrag = 3.11f;
+	public float swimmingDrag = 5.1f;
+
+	public float normalMass = 0.42f;
+	public float swimmingMass = 0.2f;
+
 	Vector2 _inputAxes;
 
 	public float speed;
 	
 
 	// Use this for initialization
-	void Start () {
-
+	void Start () 
+	{
 		_rigidBody = GetComponent<Rigidbody2D> ();
 		_inputAxes = new Vector2 ();
+		StartWalking ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 
 		_inputAxes.x = Input.GetAxis ("Horizontal");
 		_inputAxes.y = Input.GetAxis ("Vertical"); 
 
 		_rigidBody.AddForce (_inputAxes*speed);
+
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			StartSwimming();
+		}
 	
+	}
+
+	public void StartSwimming()
+	{
+		_rigidBody.mass = swimmingMass;
+		_rigidBody.drag = swimmingDrag;
+	}
+
+	public void StartWalking()
+	{
+		_rigidBody.mass = normalMass;
+		_rigidBody.drag = normalDrag;
 	}
 }
---
+++ Assets/Scripts/ComponentExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Component extensions.
+/// </summary>
+public static  class ComponentExtensions 
+{
+
+	/// <summary>
+	/// Clones a component.
+	/// </summary>
+	/// <returns>The new copy of the component</returns>
+	/// <param name="component">The component this is called from</param>
+	/// <param name="other">Where we copy the properties from.</param>
+	/// <typeparam name="T">The type of component</typeparam>
+	public static T GetCopyOf<T>(this Component component, T other) where T : Component
+	{
+		Type type = component.GetType();
+
+		// Type missmatch
+		if (type != other.GetType ())
+		{
+			Debug.LogWarning("Tried to copy " +  other.GetType().ToString() + "on to "+ type.ToString());
+			return null; 
+		}
+
+
+		BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | 
+				BindingFlags.Default | BindingFlags.DeclaredOnly;
+
+		PropertyInfo[] propertInfos = type.GetProperties(flags);
+
+		//Look through all the properties
+		foreach (var propertyInfo in propertInfos) 
+		{
+			if (propertyInfo.CanWrite) 
+			{
+				try 
+				{
+					//Copy the property.
+					propertyInfo.SetValue(component, propertyInfo.GetValue(other, null), null);
+				}
+				// In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+				catch 
+				{
+				} 
+			}
+		}
+
+		//Copy fields.
+		FieldInfo[] finfos = type.GetFields(flags);
+		foreach (var finfo in finfos) 
+		{
+			finfo.SetValue(component, finfo.GetValue(other));
+		}
+
+		return component as T;
+	}
+
+}
