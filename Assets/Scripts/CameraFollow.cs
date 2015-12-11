b3b0f6bf4b7654e905fa286f52b3dfdf326f970f
Leo Mahon <lm1249@york.ac.uk>
2015-12-11 02:57:33 +0000

    Camera Movement Improved.
    Camera now tracks X and Y directions separately.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,40 +5,42 @@ public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
 
-	public float maxSpeed = 5.0f;
 
-	public float acceleration = 2.0f;
+	private float minimumDistanceX = 20.0f;
+	private float minimumDistanceY = 5.0f;
 
-	private Vector2 _velocity;
-
-	private float minimumDistance = 20.0f;
+	private float trackSpeedX = 1.0f;
+	private float trackSpeedY = 2.0f;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
-		Vector3 deltaX3D = target.position - transform.position;
+		Vector3 newPosition = transform.position;
 
-		if (deltaX3D.magnitude > minimumDistance) {
-
-			Vector2 deltaX = new Vector2 (deltaX3D.x, deltaX3D.y);
-
+		Vector3 targetDisplacement = target.position - transform.position;
 
 
-			_velocity = deltaX.normalized * maxSpeed;
 
-			Vector3 newPosition = Vector3.Lerp(transform.position, target.position, Time.deltaTime);//transform.position;
-			newPosition.z = transform.position.z;
-
-			//newPosition.x += _velocity.x * Time.deltaTime;
-			//newPosition.y += _velocity.y * Time.deltaTime;
-	    
-			transform.position = newPosition;
+		if (Mathf.Abs(targetDisplacement.x) > minimumDistanceX)
+		{
+			newPosition.x = Mathf.Lerp (transform.position.x, target.position.x, Time.deltaTime*trackSpeedX);
 		}
+
+		if (Mathf.Abs(targetDisplacement.y) > minimumDistanceY)
+		{
+			newPosition.y = Mathf.Lerp (transform.position.y, target.position.y, Time.deltaTime*trackSpeedY);
+		}
+
+	    
+		transform.position = newPosition;
+		
 	}
 
 	public void SetTarget(Transform target)
