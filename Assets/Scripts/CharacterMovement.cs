dd3b3cc4d29159d4f09052f923b3920e3d37092f
Leo Mahon <leo_mahon@hotmail.com>
2015-12-30 22:22:44 +0000

    Sword Mechanics Change
    Changed to swiping in the direction you click.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,86 +2,86 @@
 using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
-
+	
 	Rigidbody2D _rigidBody;
-
+	
 	Vector2 _inputAxes;
-
+	
 	bool _swimming;
 	bool _inWater;
 	
-
+	
 	public float normalDrag = 10f;
 	public float normalMass = 0.1f;
-
+	
 	public float swimmingDrag = 1.11f;
 	public float swimmingMass = 0.42f;
 	
 	public float speed = 160.7f;
-
+	
 	//How long we slow down for when slowed.
 	public float slowTime = 1.5f;
-
+	
 	//How much we slow down when slowed.
 	public float slowPercentage  = 0.4f;
-
+	
 	public GameObject slowEffect;
-
+	
 	float slowTimeElapsed = 0.0f;
 	bool currentlySlowed;
-
+	
 	void Start () 
 	{
 		_inputAxes = new Vector2 ();
 		StartWalking ();
 	}
-
+	
 	void Awake()
 	{
 		_rigidBody = GetComponent<Rigidbody2D> ();
 	}
-
-
+	
+	
 	void Update()
 	{
 	}
-
+	
 	void Slow()
 	{
 		currentlySlowed = true;
 		slowEffect.SendMessage("StartEffect");
 		slowTimeElapsed = 0.0f;
 	}
-
+	
 	void FixedUpdate()
 	{
 		_inputAxes.x = Input.GetAxis ("Horizontal");
 		_inputAxes.y = Input.GetAxis ("Vertical"); 
-
-
+		
+		
 		Vector2 force = _inputAxes * speed;
-
-
-
+		
+		
+		
 		if (slowTimeElapsed > slowTime)
 		{
 			currentlySlowed = false;
 			slowEffect.SendMessage("StopEffect");
 		} 
-
+		
 		slowTimeElapsed += Time.deltaTime;
-
-
+		
+		
 		if (currentlySlowed)
 		{
 			force *= slowPercentage;
 		}
-
+		
 		_rigidBody.AddForce (force);
 	}
-
-
-
+	
+	
+	
 	public void StartSwimming()
 	{
 		_rigidBody.mass = swimmingMass;
@@ -89,7 +89,7 @@ public class CharacterMovement : MonoBehaviour {
 		_swimming = true;
 		gameObject.BroadcastMessage ("StartedSwimming");
 	}
-
+	
 	public void StartWalking()
 	{
 		_rigidBody.mass = normalMass;
@@ -97,8 +97,8 @@ public class CharacterMovement : MonoBehaviour {
 		_swimming = false;
 		gameObject.BroadcastMessage ("StartedWalking");
 	}
-
-
+	
+	
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "water")
---
+++ Assets/Scripts/SwordSlashController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class SwordSlashController : MonoBehaviour 
+{
+
+	public GameObject sword;
+	SpriteRenderer _sprite;
+
+	bool directionFlip;
+
+	void Start()
+	{
+		_sprite = sword.GetComponent<SpriteRenderer> ();
+		DisableSword ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1"))
+		{
+			Swing();
+		}
+
+	}
+
+	private void EnableSword()
+	{
+		sword.SetActive (true);
+		_sprite.DOFade (1, 0.05f);
+	}
+
+	private void DisableSword()
+	{
+		sword.SetActive(false);
+		_sprite.DOFade (0, 0.05f);
+	}
+
+	void Swing()
+	{
+		var ourScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		var direction = Input.mousePosition - ourScreenPosition; 
+		
+		float mouseAngle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+
+		float current = transform.rotation.z;
+
+
+		float startAngle = mouseAngle - 70f;
+		float finishAngle = mouseAngle + 70f;
+
+		if (directionFlip)
+		{
+			float temp = startAngle;
+			startAngle = finishAngle;
+			finishAngle = temp;
+			directionFlip = false;
+		}
+		else
+		{
+			directionFlip = true;
+		}
+
+
+		transform.rotation = Quaternion.Euler( new Vector3(0,0,startAngle));
+
+		EnableSword ();
+
+		transform.DORotate (new Vector3 (0, 0, finishAngle), 0.15f, RotateMode.Fast)
+			.SetEase (Ease.OutQuad)
+			.OnComplete (() => {
+					DisableSword();
+			});;
+	}
+}
