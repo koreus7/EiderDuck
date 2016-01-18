a304f057d072b822f346d5c7364b8ea052979aba
ramzizzle <cr902@york.ac.uk>
2016-01-18 20:40:25 +0000

    rand objects

---
+++ Assets/Scripts/CannonBallImpact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBallImpact : MonoBehaviour {
+	public float damage = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.gameObject.name == "Player")
+		{
+			coll.gameObject.SendMessage ("TakeDamage", damage);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/CannonBallSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBallSpawner : MonoBehaviour {
+
+	public Rigidbody2D ball ;
+	public float timer = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer <= 0.0f)
+		{
+			Vector3 cannonSpawn = this.transform.position;
+			Vector3 dire = (PlayerProperties.Position - cannonSpawn);
+			Rigidbody2D CBall = Instantiate (ball, cannonSpawn, Quaternion.identity) as Rigidbody2D;
+			CBall.AddForce (dire.normalized * 200);
+			timer = 0.5f;
+		} else
+		{
+			timer -= Time.deltaTime; 
+		}
+	}
+}
