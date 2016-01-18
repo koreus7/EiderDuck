006df792014d69bf58aa9601da2b450beb628631
Leo Mahon <leo_mahon@hotmail.com>
2016-01-18 22:13:15 +0000

    Made Cannon Balls Face Player

--- Assets/Scripts/CannonBallSpawner.cs
+++ Assets/Scripts/CannonBallSpawner.cs
@@ -16,8 +16,11 @@ public class CannonBallSpawner : MonoBehaviour {
 		{
 			Vector3 cannonSpawn = this.transform.position;
 			Vector3 dire = (PlayerProperties.Position - cannonSpawn);
-			Rigidbody2D CBall = Instantiate (ball, cannonSpawn, Quaternion.identity) as Rigidbody2D;
-			CBall.AddForce (dire.normalized * 200);
+			dire.Normalize();
+			var angle = Quaternion.Euler (0, 0, Mathf.Atan2 (-dire.y, -dire.x) * Mathf.Rad2Deg);
+
+			Rigidbody2D CBall = Instantiate (ball, cannonSpawn, angle) as Rigidbody2D;
+			CBall.AddForce (dire * 200);
 			timer = 0.5f;
 		} else
 		{
