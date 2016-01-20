0488c99a93b6146bbcad5c8bd6c55ee1f5f061d7
ap1278 <ap1278@york.ac.uk>
2016-01-20 10:48:48 +0000

    t
    t

--- Assets/Scripts/TextBoxManager.cs
+++ Assets/Scripts/TextBoxManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditor;
+//using UnityEditor;
 using UnityEngine.UI; // allows to use Text type
 
 public class TextBoxManager : MonoBehaviour {
